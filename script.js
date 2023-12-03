8b02b8066f07d1ac14365c263f0d725f21e5291e
Laxmi <91832050+LaxmiMaurya@users.noreply.github.com>
2023-12-03 19:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function expandCard(panelNumber) {
+    // Remove 'active' class from all panels
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+
+    // Add 'active' class to the clicked panel
+    const clickedPanel = document.getElementById(`panel-${panelNumber}`);
+    clickedPanel.classList.add('active');
+}
